0e153da3b2de4e56b312192f76935ed5027a1c23
Jungmi Ahn <quinn_akrso@kaist.ac.kr>
2019-07-09 15:00:04 -0700

    add function for tab the card

--- Rinder/app/src/main/java/com/cmps115/rinder/CardStackAdapter.kt
+++ Rinder/app/src/main/java/com/cmps115/rinder/CardStackAdapter.kt
@@ -8,24 +8,48 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import android.content.Context
+import android.content.Intent
 
-class CardStackAdapter(
-        private var spots: List<Spot> = emptyList()
+
+
+internal class CardStackAdapter(
+    context: Context, private var spots: List<Spot> = emptyList()
 ) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>() {
 
+    internal inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.item_name)
+        var f_category: TextView = view.findViewById(R.id.item_city)
+        var image: ImageView = view.findViewById(R.id.item_image)
+    }
+
+    private val context: Context
+
+    init {
+        this.context = context
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
         val inflater = LayoutInflater.from(parent.context)
         return ViewHolder(inflater.inflate(R.layout.item_spot, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val spot = spots[position]
-        holder.name.text = "${spot.id}. ${spot.name}"
+        holder.name.text = "${spot.name}"
         holder.f_category.text = spot.f_category
         Glide.with(holder.image)
                 .load(spot.url)
                 .into(holder.image)
         holder.itemView.setOnClickListener { v ->
+
+            val intent = Intent(context, InfoActivity::class.java).apply {
+                putExtra("keyIdentifier", spot.name)
+            }
+            context.startActivity(intent)
+
             Toast.makeText(v.context, spot.name, Toast.LENGTH_SHORT).show()
         }
     }
@@ -42,10 +66,5 @@ class CardStackAdapter(
         return spots
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val name: TextView = view.findViewById(R.id.item_name)
-        var f_category: TextView = view.findViewById(R.id.item_city)
-        var image: ImageView = view.findViewById(R.id.item_image)
-    }
 
 }
---
+++ Rinder/app/src/main/java/com/cmps115/rinder/InfoActivity.kt
@@ -0,0 +1,39 @@
+package com.cmps115.rinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_info.*
+import android.content.Intent
+
+class InfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+        setupButton()
+        setupInfo()
+    }
+
+
+    private fun setupInfo(){
+        if (intent.hasExtra("keyIdentifier")) {
+            spot_name.text = intent.getStringExtra("keyIdentifier")
+
+
+        } else {
+            Toast.makeText(this, "전달된 이름이 없습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun setupButton(){
+        val goBack = findViewById<View>(R.id.back_button)
+        goBack.setOnClickListener {
+            val nextIntent = Intent(this, MainActivity::class.java)
+            startActivity(nextIntent)
+        }
+    }
+
+}
